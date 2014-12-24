2e4c852cea28001987089c30e93815e3f083ceda
openminder <martin.koehn@googlemail.com>
2014-12-24 12:11:40 +0100

    add some comments

--- stacker/stacker.go
+++ stacker/stacker.go
@@ -7,10 +7,12 @@ import (
 
 func main() {
 	var haystack stack.Stack
+	// push different types of objects into the stack. the stack interface allows any type
 	haystack.Push("hay")
 	haystack.Push(-15)
 	haystack.Push([]string{"pin", "clip", "needle"})
 	haystack.Push(81.52)
+	// Print items till stack is empty - for loop without params runs infinit till break
 	for {
 		item, err := haystack.Pop()
 		if err != nil {
